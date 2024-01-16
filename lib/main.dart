b5c5d601a0f504174cae9d2f77a317cfbfdcebd0
Nur-farah <nuurfaarax15@gmail.com>
2024-01-16 21:21:03 +0300

    changed app icon and added image to the front page

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,13 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I am Rich'),
+          title: const Text('I am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey,
-        body: Center(
+        body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
         // body: Align(
@@ -21,6 +20,7 @@ void main() {
         //       'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'),
         // ),
       ),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
