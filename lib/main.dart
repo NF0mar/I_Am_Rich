2f1fcf10d225af91bf259706ed1de8b9c7c07001
Nur-farah <nuurfaarax15@gmail.com>
2024-01-17 17:58:38 +0300

    centerized title

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('I am Rich'),
+          centerTitle: true,
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey,
